d51ce4d472c54a08fbc9bc42b6a2c64b15c3e57e
Justin-Aji <143060354+Justin-Aji@users.noreply.github.com>
2023-08-24 11:53:53 +0530

    Update fcfs.c

--- CPU SCHEDULING/fcfs.c
+++ CPU SCHEDULING/fcfs.c
@@ -1,43 +1,54 @@
-#include<stdio.h>
-void main(){
-int pid[20],bt[20],n,i;
-printf(" Enter the no of processes");
-scanf("%d",&n);
-int wait[n],tat[n];
-printf("Enter the processid of processess");
-for(i=0;i<n;i++){
-scanf(" %d",&pid[i]);
-}
-printf(" Enter the burst time");
-for(i=0;i<n;i++){
-scanf(" %d",bt[i]);
-}
-float tavg,wavg,toturn,totwait;
-wait[0]=0;
-tat[0]=bt[0];
-for(i=1;i<n;i++){
-tat[i]=bt[i]+tat[i-1];
-}
-for(i=1;i<n;i++){
-wait[i]=tat[i]-bt[i];
-}
-for(i=0;i<n;i++){
-toturn+=tat[i];
-}
-for(i=0;i<n;i++){
-totwait+=wait[i];
-}
-tavg=(toturn/n);
-wavg=(totwait/n);
-printf(" Gantt chart");
-for(i=0;i<n;i++){
-printf(" p%d\t",pid[i]);
-}
-printf("\n");
-for(i=0;i<n;i++){
-printf(" %d\t",tat[i]);
-}
-printf("\n");
-printf("average turn around time:%f\t",tavg);
-printf("\n average waiting time:%f\n",wavg);
+#include <stdio.h>
+struct Process {
+	int Name;
+	int Burst_Time;
+	int Arrival_Time;
+	int Completion_Time;
+	int Turn_Around_Time;
+	int Waiting_Time;
+};
+
+int main() {
+	int n, current_ct=0;
+	float total_tat=0, total_wt=0, avg_tat, avg_wt;
+	printf("Enter the number of process : ");
+	scanf("%d", &n);
+	struct Process p[n];
+
+	for(int i=0; i<n; i++) {
+		p[i].Name = i;
+		printf("Enter the Burst Time of P%d : ", i);
+		scanf("%d", &p[i].Burst_Time);
+		p[i].Arrival_Time = 0;
+		current_ct += p[i].Burst_Time;
+		p[i].Completion_Time = current_ct;
+		p[i].Turn_Around_Time = p[i].Completion_Time;
+		p[i].Waiting_Time = p[i].Turn_Around_Time - p[i].Burst_Time;
+	}
+
+	printf("\n      Burst Time\tTurn Around Time\tWaiting Time\n");
+	for(int i=0; i<n; i++) {
+		printf("P%d    %d\t\t\t%d\t\t\t%d\n", p[i].Name, p[i].Burst_Time, p[i].Turn_Around_Time, p[i].Waiting_Time);
+		total_tat += p[i].Turn_Around_Time;
+		total_wt += p[i].Waiting_Time;
+	}
+	printf("\n");
+
+	printf("Gantt Chart\n");
+	printf("|");
+	for(int i=0; i<n; i++) {
+		printf("  P%d  |", p[i].Name);
+	}
+	printf("\n");
+	printf("0      ");
+	for(int i=0; i<n; i++) {
+		printf("%d      ", p[i].Completion_Time);
+	}
+	printf("\n\n");
+
+	printf("Total Turn Around Time : %f\n", total_tat);
+	printf("Total Waiting Time : %f\n", total_wt);
+	printf("Average Turn Around Time : %f\n", total_tat/n);
+	printf("Average Waiting Time : %f\n\n", total_wt/n);
+
 }
